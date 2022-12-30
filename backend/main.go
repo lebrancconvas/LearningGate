914bef516240778046f9fc76bf2e8f6397ce8121
lebrancconvas <lebrancconvas@gmail.com>
2022-12-30 23:28:58 +0700

    ✨ feat: create router

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,19 @@
 package main 
 
 import (
-	
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
+
+func getIndex(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello World",
+	})
+}
 
 func main() {
 	// Start the server 
+	router := gin.Default();
+	router.GET("/", getIndex);
+	router.Run(":8000"); 
 }
