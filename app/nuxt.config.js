4be079427147745e68185f3f3bd13ae7642f2698
lebrancconvas <lebrancconvas@gmail.com>
2022-12-30 22:31:19 +0700

    🚚 refactor: clean starter code

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - app',
-    title: 'app',
+    titleTemplate: '%s',
+    title: 'Learning Gate | Learning Social for not being Ignorant',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
