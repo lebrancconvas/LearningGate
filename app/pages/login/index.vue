49bdf6a7ae4ead6d1aee85d61f028cb424d02213
lebrancconvas <lebrancconvas@gmail.com>
2022-12-31 11:20:53 +0700

    ✨ feat: create login page

---
+++ app/pages/login/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <h1>Login</h1>
+      </div>
+    </header>
+    <section id="login-section">
+      <div>
+        <v-form lazy-validation>
+          <v-text-field v-model="loginForm.username" label="Username" required></v-text-field>
+          <v-text-field v-model="loginForm.password" type="password" label="Password" required></v-text-field>
+          <div id="action">
+            <v-btn color="primary" @click="login">Login</v-btn>
+          </div>
+        </v-form>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loginForm: {
+          username: "",
+          password: ""
+        }
+      }
+    },
+    methods: {
+      login() {
+        console.log(this.loginForm);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  * {
+    text-align: center;
+  }
+
+  #login-section {
+    width: 50%;
+    margin: 0 auto;
+  }
+</style>
