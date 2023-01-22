99a0e23375cf41cd9d90cd22af9b9af25ad3b0a4
lebrancconvas <lebrancconvas@gmail.com>
2023-01-22 23:48:39 +0700

    ✨ feat: add register and login button on landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,16 +4,41 @@
       <div>
         <h1>Learning Gate</h1>
       </div>
+      <div>
+        <h2>Platform for Learning Social Topics to improve yourself and society</h2>
+      </div>
     </header>
+    <section id="action-section">
+      <div>
+        <h3>Register Now before you'll miss the great opportunity :)</h3>
+      </div>
+      <div>
+        <v-btn color="primary" @click="toRegister">Register</v-btn>
+        <v-btn color="success" @click="toLogin">Login</v-btn>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-
+  export default {
+    methods: {
+      toRegister() {
+        this.$router.push({ path: '/register' });
+      },
+      toLogin() {
+        this.$router.push({ path: '/login' });
+      }
+    }
+  }
 </script>
 
 <style scoped>
   * {
     text-align: center;
+  }
+
+  #action-section {
+    margin: 20px;
   }
 </style>
